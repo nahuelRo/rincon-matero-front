644fca92952b48245504a7f62cb11e4f06f320bc
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-09-08 10:59:16 -0300

    create register component

---
+++ src/hooks/useInput.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const useInput = () => {
+  const [value, setValue] = useState("");
+
+  const onChange = ({ target }) => {
+    setValue(target.value);
+  };
+
+  return { value, onChange };
+};
+
+export default useInput;
