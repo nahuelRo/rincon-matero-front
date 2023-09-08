1d6947c89503bc6e3008550d02f714eed7aa99ad
Braian Barrientos <braianbts@gmail.com>
2023-09-08 11:05:04 -0300

    Navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./styles/styles.scss";
 import NavbarComponent from "./components/Navbar";
-import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
-    <NextUIProvider>
+    <>
       <NavbarComponent />
       <h1>Rincon Matero</h1>
-    </NextUIProvider>
+    </>
   );
 }
 
