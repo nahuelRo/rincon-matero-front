fa562d759847cc79df545e054805df31020a4578
Braian Barrientos <braianbts@gmail.com>
2023-09-12 09:31:22 -0300

    bug carrousel fixed

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./carrousel.module.scss";
 import carrousel from "../../assets/bannner1.png";
-import { Carousel } from "react-responsive-carousel";
+import carrousel2 from "../../assets/banner2.png";
 
-const Carrousel = () => {
+function Carrousel() {
+  const [imagenActual, setImagenActual] = useState(1);
+
+  // Función para cambiar la imagen
+  const cambiarImagen = (direccion) => {
+    if (direccion === "anterior") {
+      setImagenActual(imagenActual === 1 ? 2 : 1);
+    } else {
+      setImagenActual(imagenActual === 2 ? 1 : 2);
+    }
+  };
+
+  // Función para cambiar automáticamente la imagen cada 2 segundos
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cambiarImagen("siguiente");
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [imagenActual]);
+
   return (
-    <div>
+    <div className={styles.carrusel}>
       <img
-        src={carrousel}
-        alt="carrousel"
-        style={{ width: "100%", marginBottom: "20px" }}
+        src={imagenActual === 1 ? carrousel : carrousel2}
+        alt={`Imagen ${imagenActual}`}
       />
     </div>
   );
-};
+}
 
 export default Carrousel;
